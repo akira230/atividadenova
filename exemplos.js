e52586e644c54be98c985b440aeaaccefe5683d7
akira <yasmimangelicadejesus65@gmail.com>
2026-09-01 09:36:20 -0300

    Create exemplos.js

---
+++ exemplos.js
@@ -0,0 +1,35 @@
+//exemplo 1
+
+let valor1 = 20;
+let valor2 = 5;
+
+console.log(valor1 + valor2);
+console.log(valor1 - valor2);
+console.log(valor1 * valor2);
+console.log(valor1 / valor2);
+console.log(10%3);
+
+// aritmética são os comandos de soma, nultiplicação, subtração e dividida
+
+console.log("exemplo 3")
+
+//exempos 2
+
+console.log(10%3);
+
+console.log("exemplo 3")
+
+console.log(10>5); //verdadeiro
+console.log(10<5);//Falsa
+console.log(10>=5);//verdadeiro
+console.log(10===5);//verdadeiro
+console.log(10!==5);//verdadeiro
+
+console.log("exemplo 4")
+console.log(10>=10&&20===20)
+
+console.log("exemplo 5")
+console.log(15>20||8<10)
+
+console.log("exemplo 6")
+console.log(!(20>10));
